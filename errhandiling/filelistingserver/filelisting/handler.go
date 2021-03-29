f42cd2a79a905fe3516f9e68c2495d18136a8e11
nirolee <nirolee@foxmail.com>
2021-03-29 14:23:18 +0800

    统一的错误处理

---
+++ errhandiling/filelistingserver/filelisting/handler.go
@@ -0,0 +1,26 @@
+package filelisting
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func HandleFileList(writer http.ResponseWriter, request *http.Request) {
+	path := request.URL.Path[len("/list/"):]
+	file, err := os.Open(path)
+	if err != nil {
+		http.Error(writer,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	all, err := ioutil.ReadAll(file)
+	if err != nil {
+		http.Error(writer,
+			err.Error(),
+			http.StatusInternalServerError)
+	}
+	writer.Write(all)
+}
---
+++ errhandiling/filelistingserver/web.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"gin/errhandiling/filelistingserver/filelisting"
+	"net/http"
+	"os"
+)
+
+type appHandler func(writer http.ResponseWriter,
+	request *http.Request) error
+
+func errWrapper(handler appHandler) func(
+	http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := handler(writer, request)
+		if err != nil {
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				http.Error(writer,
+					http.StatusText(http.StatusNotFound),
+					http.StatusNotFound)
+			case os.IsPermission(err):
+				http.Error(writer,
+					http.StatusText(http.StatusForbidden),
+					http.StatusForbidden)
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer,
+				http.StatusText(code), code)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/list/", filelisting.HandleFileList)
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
