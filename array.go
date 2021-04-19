5d1e78cded380f76277c39c2b2d98ec1c8f30d09
nirolee <nirolee@foxmail.com>
2021-04-19 09:39:14 +0800

    test driven development

--- array.go
+++ array.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+var (
+	size     = 1024
+	max_size = size * 2
+)
+
+type a struct {
+	age  int
+	name string
+}
+
+type b struct {
+	age  int
+	name string
+}
+
+type Int1 int
+type Int2 = int
 
 func main() {
-	s1 := []int{1, 2, 3}
-	s2 := []int{1, 2, 4}
-	s1 = append(s1, s2[:]...)
-	fmt.Println(s1)
+	//s1 := []int{1, 2, 3}
+	//s2 := []int{1, 2, 4}
+	//s1 = append(s1, s2[:]...)
+	//fmt.Println(size, max_size)
+	//c := a{1, "1"}
+	//d := a{1, "1"}
+	//if c==d {
+	//	println(1)
+	//}
+	//var i int = 2
+	//var i2 Int1 = i
+	//var i2 Int2 = i
+	//fmt.Println(i2)
+
 }
--- map.go
+++ map.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+type person struct {
+	name string
+}
+
 func main() {
 	//var j = map[int]string{
 	//	1:"1j",
@@ -11,12 +15,18 @@ func main() {
 	//}
 	//fmt.Println(j)
 
-	slice := []int{0, 1, 2, 3}
-	m := make(map[int]*int)
-	for key, value := range slice {
-		m[key] = &value
-	}
-	for k, v := range m {
-		fmt.Println(k, "->", *v)
-	}
+	//slice := []int{0, 1, 2, 3}
+	//m := make(map[int]*int)
+	//for key, value := range slice {
+	//	m[key] = &value
+	//}
+	//for k, v := range m {
+	//	fmt.Println(k, "->", *v)
+	//}
+
+	fmt.Println(cap([4]int{1, 2, 4}))
+}
+
+func hello(num ...int) {
+	num[0] = 18
 }
